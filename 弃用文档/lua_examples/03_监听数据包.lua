f674d008e6f9a6868fb3c861bba0d8d0e80708cb
CMA2401PT <1634268014@qq.com>
2024-08-01 20:17:47 +0800

    fix packet:json_str

--- 弃用文档/lua_examples/03_监听数据包.lua
+++ 弃用文档/lua_examples/03_监听数据包.lua
@@ -26,7 +26,7 @@ while packet_poller:block_has_next() do                                  -- 如�
     -- just 10 packets as an example
     packet_count = packet_count + 1
     if packet_count > 10 and packet:name() == packets.CommandOutput then                           -- 我们只取10个数据包，然后退出
-        print(("detail packet %s"):format(packet:json_str(packet)))                                -- json_str 是一个比较慢的函数，但是它可以将数据包转换成json字符串，方便我们查看，它最好只在调试的时候使用
+        print(("detail packet %s"):format(packet:json_str()))                                -- json_str 是一个比较慢的函数，但是它可以将数据包转换成json字符串，方便我们查看，它最好只在调试的时候使用
         local packet_userdata = packet:user_data()                                                 -- user_data 可以将数据包转换成lua user_data，方便我们取其中的数据
         print(("detail packet (user_data) %s"):format(packet_userdata))                            -- lua user_data 可以直接打印, 但是诸如 ipair\pair 之类的函数对其不起作用
         print(("detail packet (lua table) %s"):format(ud2lua(packet_userdata)))                    -- 使用 ud2lua 可以将其彻底转为 lua table，ipair和pair可以使用，但是转换需要额外消耗时间，如果不需要用到 pair 和 ipair，建议不要使用
