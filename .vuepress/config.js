477de18660b54c669e281e42e2d33f9ea839a407
kukinghan <kukinghan@163.com>
2024-08-02 19:06:44 +0800

    统一配置风格

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,48 +7,46 @@ import { searchPlugin } from '@vuepress/plugin-search'
 
 // lua 接口文档全局定义
 const coromega = [
-	// 调试 lua 代码
-	'/lua插件编写/调试lua代码/调试代码/',
 	// 读取配置
-	'/lua插件编写/配置读取和升级相关API/配置读取和升级/',
+	'/lua插件编写/配置读取和升级相关API/配置读取和升级.md',
 	// 菜单
-	'/lua插件编写/菜单相关API/菜单相关API/',
+	'/lua插件编写/菜单相关API/菜单相关API.md',
 	// 命令收发
-	'/lua插件编写/命令收发api/coromega_cmd/',
+	'/lua插件编写/命令收发api/coromega_cmd.md',
 	// 机器人
-	'/lua插件编写/机器人和服务器信息/coromega_botUq/',
+	'/lua插件编写/机器人和服务器信息/coromega_botUq.md',
 	// 玩家与命令
-	'/lua插件编写/玩家交互与命令方块API/cmd_player/',
+	'/lua插件编写/玩家交互与命令方块API/cmd_player.md',
 	// botAction
-	'/lua插件编写/botAction/botAction/',
+	'/lua插件编写/botAction/botAction.md',
 	// 建造
-	'/lua插件编写/方块和命令块放置相关/coromega_place_command_block/',
+	'/lua插件编写/方块和命令块放置相关/coromega_place_command_block.md',
 	// 建筑操作
-	'/lua插件编写/建筑操作/08_structure/',
+	'/lua插件编写/建筑操作/08_structure.md',
 	// 导入器
-	'/lua插件编写/导入器_strucure_canvas相关API/导入器_strucure_canvas相关API/',
+	'/lua插件编写/导入器_strucure_canvas相关API/导入器_strucure_canvas相关API.md',
 	// 数据包
-	'/lua插件编写/数据包监听相关API/数据包监听相关API/',
+	'/lua插件编写/数据包监听相关API/数据包监听相关API.md',
 	// 跨插件通信
-	'/lua插件编写/跨插件api调用/跨插件api调用/',
+	'/lua插件编写/跨插件api调用/跨插件api调用.md',
 	// 系统功能
-	'/lua插件编写/system/coromega_system/',
+	'/lua插件编写/system/coromega_system.md',
 	// 存储
-	'/lua插件编写/存储相关/coromega_storage/',
+	'/lua插件编写/存储相关/coromega_storage.md',
 	// cqhttp
-	'/lua插件编写/cqhttp相关API/cqhttp相关API/',
+	'/lua插件编写/cqhttp相关API/cqhttp相关API.md',
 	// http
-	'/lua插件编写/http相关/http/',
+	'/lua插件编写/http相关/http.md',
 	// websocket
-	'/lua插件编写/websocket/websocket/',
+	'/lua插件编写/websocket/websocket.md',
 	// 加密哈希Base64
-	'/lua插件编写/密码_哈希_base64/密码_哈希_base64/',
+	'/lua插件编写/密码_哈希_base64/密码_哈希_base64.md',
 	// 其他
-	'/lua插件编写/其他/common/',
+	'/lua插件编写/其他/common.md',
 	// 代码分发保护
-	'/lua插件编写/在分发时保护你的代码/在分发时保护你的代码/',
+	'/lua插件编写/在分发时保护你的代码/在分发时保护你的代码.md',
 	// 软API
-	'/lua插件编写/软API/软API/',
+	'/lua插件编写/软API/软API.md',
 ]
 
 
@@ -82,7 +80,7 @@ export default defineUserConfig({
 		 * 控制显示右上角的 github 地址
 		 * 在 github 上编辑此页按钮依据此条配置生成
 		 */
-		repo: 'OmineDev/neomega-doc/',
+		repo: 'OmineDev/neomega-doc',
 		logo: "lumega.png",
 		editLinkText: "在 GitHub 上编辑此页",
 		// 最近更新的文字
@@ -105,7 +103,7 @@ export default defineUserConfig({
 			},
 			{
 				text: '编写Lua插件:接口列表',
-				link: '/lua插件编写/',
+				link: '/lua插件编写',
 				children: [
 					{
 						text: 'coromega',
@@ -115,19 +113,20 @@ export default defineUserConfig({
 		],
 		// 自定义侧边栏
 		sidebar: {
-			'/安装说明/': {
-				text: '安装 NeOmega',
-				link: '/安装说明.md',
-			},
-			'/lua插件编写/快速开始/60分钟闪电战/': {
-				text: '编写Lua插件:快速开始',
-				link: '/lua插件编写/快速开始/60分钟闪电战.md',
-			},
-			// lua插件编写
-			'/lua%E6%8F%92%E4%BB%B6%E7%BC%96%E5%86%99/': [{
+			'/安装说明':[{
+				text: '快速开始',
+				children: ['/安装说明.md']
+			}],
+			'/lua插件编写/快速开始/': [{
+				text: '快速开始',
+				children: ['/lua插件编写/快速开始/60分钟闪电战.md']
+			}],
+			'/lua插件编写/调试lua代码/': [{
+				text: '调试',
+				children: ['/lua插件编写/调试lua代码/调试代码.md']
+			}],
+			'/lua插件编写/': [{
 				text: '编写Lua插件:接口列表',
-				// lua插件编写
-				link: '/lua插件编写/',
 				children: coromega
 			}]
 		},
